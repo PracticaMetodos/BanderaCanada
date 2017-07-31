1c5b3643c6dddfa4e79fa6a87ca83c9d05b80275
LuzHf <30605408+LuzHf@users.noreply.github.com>
2017-07-31 15:40:15 -0500

    Agregar archivos de proyecto.

---
+++ BanderaCanada/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BanderaCanada.Clases;
+
+namespace BanderaCanada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Bandera laBandera = new Bandera();
+            laBandera.Mostrar();
+
+        }
+    }
+}
