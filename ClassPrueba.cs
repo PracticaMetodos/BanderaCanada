4a6e3454c5d2319847b9a346f7af6a8ea838f38f
LuzHf <30605408+LuzHf@users.noreply.github.com>
2017-07-31 16:41:38 -0500

    Add files via upload

---
+++ ClassPrueba.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BanderaCanada.Clases
+{
+    class ClassPrueba
+    {
+        public void New()
+        { Console.WriteLine("Hola"); }
+    }
+}
